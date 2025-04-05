c4297bf41e576412f571978756b7b8b7510cecca
Allan Tom-Wolverton <atomwolve@gmail.com>
2025-04-05 04:52:47 +0000

    Started messages DAO

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,11 @@
 package DAO;
 import Model.Account;
+import Util.ConnectionUtil;
 public class AccountDAO {
+
+    //Register a new user
+    //Process login
+
+
 
 }
--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,6 +1,84 @@
 package DAO;
 import Model.Message;
+import Util.ConnectionUtil;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
 
-public class MessageDAO {
+public class MessageDAO 
+{
+    //Process creation of new message
+    //Retrieve all messages
+    /**
+     * @return all messages.
+     */
+    public List<Message> getAllMessages(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM message";
 
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                Message message = new Message(
+                    rs.getInt("message_id"),
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+
+    //Retrieve message by ID
+    public Message getMessageByID()
+    {
+        return null;
+    }
+    //Delete message by ID
+    public void deleteMessageByID()
+    {
+
+    }
+    //Update message by ID
+    public void updateMessageByID()
+    {
+
+    }
+    //Retrieve all messages by user
+    public List<Message> getAllMessagesByAccount(int account_id)
+    {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM message WHERE posted_by == ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, account_id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                Message message = new Message(
+                    rs.getInt("message_id"),
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch"));
+                messages.add(message);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return messages;
+    }
+    //
 }
