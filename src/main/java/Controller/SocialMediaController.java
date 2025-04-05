0832f09d689eabcc02784e505d046d47bd1d43b6
Allan Tom-Wolverton <atomwolve@gmail.com>
2025-04-05 05:25:50 +0000

    started hooking up controller

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,13 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import Service.AccountService;
+import Service.MessageService;;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -9,6 +15,15 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+
+    AccountService accountService;
+    MessageService messageService;
+    
+    public SocialMediaController(){
+        accountService = new AccountService();
+        messageService = new MessageService();
+    }
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -16,18 +31,72 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("localhost:8080/register", this::postAccountHandler);   //TODO: check if localhost is needed
+        // app.put("localhost:8080/login", this::updateFlightHandler); //TODO: check if {login creds} is needed
+        app.post("localhost:8080/messages", this::postMessageHandler);
+        app.get("localhost:8080/messages", this::getAllMessagesHandler);
+        app.get("localhost:8080/messages/{message_id}", this::getMessageByIDHandler);
+        app.get("localhost:8080/accounts/{account_id}/messages", this::getAllMessagesByUserHandler);
 
+        app.patch("localhost:8080/messages/{message_id}", this::updateMessageHandler);
+        app.delete(("localhost:8080/messages/{message_id}"), this::deleteMessageHandler);
         return app;
     }
 
+    private void postAccountHandler(Context ctx) throws JsonProcessingException
+    {
+
+    }
+    private void deleteMessageHandler(Context ctx){
+
+    }
+    private void updateMessageHandler(Context ctx){
+
+    }
+    private void getAllMessagesHandler(Context ctx){
+        // ctx.json(messageService.getAllMessages());
+    }
+    private void getAllMessagesByUserHandler(Context ctx){
+        // ctx.json(messageService.getAllMessages());
+    }
+    private void getMessageByIDHandler(Context ctx){
+        // ctx.json(messageService.getAllMessages());
+    }
+    private void postMessageHandler(Context ctx){
+
+    }
     /**
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    private void postFlightHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Flight flight = mapper.readValue(ctx.body(), Flight.class);
+        Flight addedFlight = flightService.addFlight(flight);
+        if(addedFlight==null){
+            ctx.status(400);
+        }else{
+            ctx.json(mapper.writeValueAsString(addedFlight));
+        }
     }
+    private void updateFlightHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Flight flight = mapper.readValue(ctx.body(), Flight.class);
+        int flight_id = Integer.parseInt(ctx.pathParam("flight_id"));
+        Flight updatedFlight = flightService.updateFlight(flight_id, flight);
+        System.out.println(updatedFlight);
+        if(updatedFlight == null){
+            ctx.status(400);
+        }else{
+            ctx.json(mapper.writeValueAsString(updatedFlight));
+        }
 
-
+    }
+    private void getAllFlightsHandler(Context ctx){
+        ctx.json(flightService.getAllFlights());
+    }
+    private void getAllFlightsDepartingFromCityArrivingToCityHandler(Context ctx) {
+        ctx.json(flightService.getAllFlightsFromCityToCity(ctx.pathParam("departure_city"),
+                ctx.pathParam("arrival_city")));
+    }
 }
--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,7 +1,6 @@
 package DAO;
 import Model.Message;
 import Util.ConnectionUtil;
-import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.*;
@@ -9,7 +8,68 @@ import java.sql.*;
 public class MessageDAO 
 {
     //Process creation of new message
-    //Retrieve all messages
+    
+    public void insertMessage(Message message)
+    {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES(?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, message.posted_by);
+            preparedStatement.setString(2, message.message_text);
+            preparedStatement.setLong(3, message.time_posted_epoch);
+
+            preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // message_id integer primary key auto_increment,
+    // posted_by integer,
+    // message_text varchar(255),
+    // time_posted_epoch long,
+    // foreign key (posted_by) references Account(account_id)
+    //Delete message by ID
+    public void deleteMessageByID(int message_id)
+    {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Write SQL logic here
+            String sql = "DELETE FROM message WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, message_id);
+
+            preparedStatement.executeUpdate();  //TODO: check if correct syntax
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Update message by ID
+    public void updateMessageByID(int message_id, String message_text)
+    {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Write SQL logic here
+            String sql = "UPDATE message SET message_text = ? WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, message_text);
+            preparedStatement.setInt(2, message_id);
+
+            preparedStatement.executeUpdate();
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+//Retrieve all messages
     /**
      * @return all messages.
      */
@@ -17,7 +77,6 @@ public class MessageDAO
         Connection connection = ConnectionUtil.getConnection();
         List<Message> messages = new ArrayList<>();
         try {
-            //Write SQL logic here
             String sql = "SELECT * FROM message";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -38,20 +97,34 @@ public class MessageDAO
     }
 
     //Retrieve message by ID
-    public Message getMessageByID()
+    /**
+     * @return message by message_id
+     */
+    public Message getMessageByID(int message_id)
     {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM message WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message_id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next())
+            {
+                Message message = new Message(
+                    rs.getInt("message_id"),
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch"));
+                return message;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
-    //Delete message by ID
-    public void deleteMessageByID()
-    {
 
-    }
-    //Update message by ID
-    public void updateMessageByID()
-    {
-
-    }
     //Retrieve all messages by user
     public List<Message> getAllMessagesByAccount(int account_id)
     {
@@ -59,7 +132,7 @@ public class MessageDAO
         List<Message> messages = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "SELECT * FROM message WHERE posted_by == ?";
+            String sql = "SELECT * FROM message WHERE posted_by = ?";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
@@ -80,5 +153,4 @@ public class MessageDAO
         }
         return messages;
     }
-    //
 }
